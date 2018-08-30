fa87ddda26c951e5b6639dbd71483c3b1fd3b5ce
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-30 15:44:46 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
     if params[:id]
       @song = Song.find_by(id: params[:id])
       if @song == nil
-        redirect_to artists_path
+        redirect_to artists_songs_path, alert: "Song not found"
       end
     else
       @songs = Song.all
