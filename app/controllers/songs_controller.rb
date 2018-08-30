9dbd31cacdea4f8b78d6918398e9d70f0f6463c1
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-30 15:38:07 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
 
 
   def show
-    if params[:artist_id]
+    if params[:id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist == nil
         redirect_to artists_path
